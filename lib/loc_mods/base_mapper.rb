b9caca8a53561c2c4a5f7ecd7f6f02d072056670
Ronald Tse <ronald.tse@ribose.com>
2024-07-14 15:33:07 +0800

    chore: update comparison print outs

--- lib/loc_mods/base_mapper.rb
+++ lib/loc_mods/base_mapper.rb
@@ -3,7 +3,23 @@ require "shale"
 require_relative "comparable_mapper"
 
 module LocMods
+
+  # Base class for all object definitions
   class BaseMapper < Shale::Mapper
     include ComparableMapper
+  end
+
+  # Nil class substitute for comparison
+  class ComparableNil < BaseMapper
+  end
+
+  # Comparison of two values for ComparableMapper
+  class Comparison
+    attr_accessor :original, :updated
+
+    def initialize(original:, updated:)
+      @original = original
+      @updated = updated
+    end
   end
 end
