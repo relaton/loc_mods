f6f978759ba006766c86725e28b53e5eb46cb0ff
Ronald Tse <ronald.tse@ribose.com>
2025-01-14 15:27:19 +0800

    fix: let lutaml-model handle schemaLocation

--- lib/loc_mods/collection.rb
+++ lib/loc_mods/collection.rb
@@ -13,10 +13,6 @@ module LocMods
       root "modsCollection"
       namespace "http://www.loc.gov/mods/v3", nil
 
-      map_attribute "schemaLocation", to: :schema_location,
-                                      namespace: "http://www.w3.org/2001/XMLSchema-instance",
-                                      prefix: "xsi"
-
       map_element "mods", to: :mods
     end
   end
